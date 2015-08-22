b9823eae396579c4323e0542c2e781dc952400a0
Adrian Gibbons <adriangibbons@users.noreply.github.com>
2015-08-22 13:04:46 +0800

    added PowerTest

---
+++ tests/pFFA-Power-Test.php
@@ -0,0 +1,84 @@
+<?php
+error_reporting(E_ALL);
+if(!class_exists('phpFITFileAnalysis')) {
+    require __DIR__ . '/../src/php-FIT-File-Analysis.php';
+}
+
+class PowerTest extends PHPUnit_Framework_TestCase
+{
+    private $base_dir;
+    private $filename = 'power-analysis.fit';
+    private $pFFA;
+    
+    public function setUp()
+    {
+        $this->base_dir = __DIR__ . '/../demo/fit_files/';
+        $this->pFFA = new phpFITFileAnalysis($this->base_dir . $this->filename, ['units' => 'raw']);
+    }
+    
+    public function testPower_critical_power_values()
+    {
+        $time_periods = [2,5,10,60,300,600,1200,1800,3600];
+        $cps = $this->pFFA->critical_power($time_periods);
+        
+        array_walk($cps, function(&$v) { $v = round($v, 2); });
+        
+        $this->assertEquals(551.50, $cps[2]);
+        $this->assertEquals(542.20, $cps[5]);
+        $this->assertEquals(527.70, $cps[10]);
+        $this->assertEquals(452.87, $cps[60]);
+        $this->assertEquals(361.99, $cps[300]);
+        $this->assertEquals(328.86, $cps[600]);
+        $this->assertEquals(260.52, $cps[1200]);
+        $this->assertEquals(221.81, $cps[1800]);
+    }
+    
+    public function testPower_critical_power_time_period_max()
+    {
+        // 14400 seconds is 4 hours and longer than file duration so should only get one result back (for 2 seconds)
+        $time_periods = [2,14400];
+        $cps = $this->pFFA->critical_power($time_periods);
+        
+        $this->assertEquals(1, count($cps));
+    }
+    
+    public function testPower_power_metrics()
+    {
+        $power_metrics = $this->pFFA->power_metrics(350);
+        
+        $this->assertEquals(221, $power_metrics['Average Power']);
+        $this->assertEquals(671, $power_metrics['Kilojoules']);
+        $this->assertEquals(285, $power_metrics['Normalised Power']);
+        $this->assertEquals(1.29, $power_metrics['Variability Index']);
+        $this->assertEquals(0.81, $power_metrics['Intensity Factor']);
+        $this->assertEquals(56, $power_metrics['Training Stress Score']);
+    }
+    
+    public function testPower_power_partitioned()
+    {
+        // Calls phpFITFileAnalysis::power_zones();
+        $power_partioned = $this->pFFA->power_partioned(350);
+        
+        $this->assertEquals(45.2, $power_partioned['0-193']);
+        $this->assertEquals(10.8, $power_partioned['194-263']);
+        $this->assertEquals(18.1, $power_partioned['264-315']);
+        $this->assertEquals(17.9, $power_partioned['316-368']);
+        $this->assertEquals(4.2, $power_partioned['369-420']);
+        $this->assertEquals(3.3, $power_partioned['421-525']);
+        $this->assertEquals(0.4, $power_partioned['526+']);
+    }
+    
+    public function testPower_power_histogram()
+    {
+        // Calls phpFITFileAnalysis::histogram();
+        $power_histogram = $this->pFFA->power_histogram(100);
+        
+        $this->assertEquals(374, $power_histogram[0]);
+        $this->assertEquals(634, $power_histogram[100]);
+        $this->assertEquals(561, $power_histogram[200]);
+        $this->assertEquals(1103, $power_histogram[300]);
+        $this->assertEquals(301, $power_histogram[400]);
+        $this->assertEquals(66, $power_histogram[500]);
+        $this->assertEquals(4, $power_histogram[600]);
+    }
+}
