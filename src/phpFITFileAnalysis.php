bb078197cd9c8eda5f3ee0475ee73fd8a10384ad
Adrian Gibbons <email@adriangibbons.com>
2017-08-15 18:32:07 +0800

    should've replaced 'goto' with 'break'

--- src/phpFITFileAnalysis.php
+++ src/phpFITFileAnalysis.php
@@ -2030,8 +2030,7 @@ class phpFITFileAnalysis
             $count = count($thresholds);
             for ($key; $key<$count; ++$key) {
                 if ($value < $thresholds[$key]) {
-                    $result[$key]++;
-                    continue;
+                    break;
                 }
             }
             $result[$key]++;
